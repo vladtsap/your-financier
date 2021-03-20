b697ab3d78ced801d57dec52ccdcfdfc689e2443
vladtsap <vldzpp@gmail.com>
2021-03-20 04:35:04 +0200

    restrict access only to admin while in development

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Dispatcher
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 from middlewares.throttling import ThrottlingMiddleware
+from middlewares.admin_only import AccessMiddleware
 
 
 def setup(dp: Dispatcher):
     dp.middleware.setup(LoggingMiddleware())
     dp.middleware.setup(ThrottlingMiddleware())
+    dp.middleware.setup(AccessMiddleware(148111610))  # TODO: remove before release
