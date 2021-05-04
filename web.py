37d6c7082fb82948d729e56d5c0c9e5a40a0705b
vladtsap <vldzpp@gmail.com>
2021-05-04 17:20:02 +0300

    use fastAPI deserializer

--- web.py
+++ web.py
@@ -1,10 +1,9 @@
 from dataclasses import replace
 from datetime import datetime
-from json import JSONDecodeError
 
 import uvicorn
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
 from pytz import timezone
 
 from config import bot
@@ -14,57 +13,53 @@ from utils.mongo import MongoTransaction
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    time: int
+    mcc: int
+    amount: int
 
 
-def process_transaction(budget_id: str, member_id: int, data: dict) -> Transaction:
+class Data(BaseModel):
+    account: str
+    statementItem: Item
+
+
+class Object(BaseModel):
+    type: str
+    data: Data
+
+
+@app.get('/webhook/{budget_id}/{member_id}')
+def test_webhook():
+    return Response()
+
+
+@app.post('/webhook/{budget_id}/{member_id}')
+async def process_transaction(budget_id: str, member_id: int, obj: Object):
+    item = obj.data.statementItem
+
     transaction = Transaction(
         budget_id=budget_id,
         member_id=member_id,
         date=timezone('UTC').localize(
-            datetime.utcfromtimestamp(data['time'])
+            datetime.utcfromtimestamp(item.time)
         ).astimezone(timezone('Europe/Kiev')),
-        category=Categories.match(data['mcc']),
+        category=Categories.match(item.mcc),
     )
 
-    if (amount := data['amount']) > 0:
+    if (amount := item.amount) > 0:
         transaction = replace(transaction, income=amount / 100)
     else:
         transaction = replace(transaction, outcome=abs(amount) / 100)
 
-    return transaction
-
-
-@app.get('/webhook/{budget_id}/{member_id}')
-def ping(budget_id: str, member_id: int):
-    return {'status': 'ok'}
-
-
-@app.post('/webhook/{budget_id}/{member_id}')
-async def webhook(budget_id: str, member_id: int, request: Request):
-    content = await request.json()
-
-    transaction = process_transaction(
-        budget_id=budget_id,
-        member_id=member_id,
-        data=content['data']['statementItem'],
-    )
-
     MongoTransaction().add(transaction)
+
     await bot.send_message(
         chat_id=member_id,
         text=transaction.message_view,
     )
 
-    # TODO: send notification to group
     return Response()
-
-
-@app.exception_handler(JSONDecodeError)
-async def handle_not_json_error(request: Request, exc: JSONDecodeError):
-    return PlainTextResponse('not JSON format', status_code=400)
 
 
 if __name__ == "__main__":
