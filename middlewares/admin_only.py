05be2458edab9c334aeda7c6075f16ad8a8c3b79
vladtsap <vldzpp@gmail.com>
2021-05-05 12:42:03 +0300

    add one more admin

--- middlewares/admin_only.py
+++ middlewares/admin_only.py
@@ -1,13 +1,15 @@
+from typing import List
+
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.types import Message
 
 
 class AccessMiddleware(BaseMiddleware):
-    def __init__(self, access_id: int):
-        self.access_id = access_id
+    def __init__(self, access_ids: List[int]):
+        self.access_ids = access_ids
         super().__init__()
 
     async def on_process_message(self, message: Message, _):
-        if message.from_user.id != self.access_id:
+        if message.from_user.id not in self.access_ids:
             raise CancelHandler()
